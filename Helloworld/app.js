15b7cd3389c31e9d9e4703195b7051b6944d57a2
Fredrik Schulte <fredrik@schulte.se>
2023-01-22 19:02:11 +0100

    Ny kod från chatGPT

--- Helloworld/app.js
+++ Helloworld/app.js
@@ -1,5 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.end('Hello World!');
-}).listen(8080);
+const http = require('http');
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
